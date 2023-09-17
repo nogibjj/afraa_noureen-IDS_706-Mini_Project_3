06ce338eac12a86f1b67cf4283c03b4c3eb1d55e
afraa-n <143756865+afraa-n@users.noreply.github.com>
2023-09-17 22:46:56 +0000

    done

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 """main module"""
-import pandas as pd
 import matplotlib.pyplot as plt
 from ydata_profiling import ProfileReport
-
+import polars as pl
 
 def display_highest_votes():
     """
     Display basic info about movies dataset
     """
-    movies_df = pd.read_csv("MoviesTopRated.csv")
-    vote_df = movies_df.query("vote_average == vote_average.max()")
+    polars_movies_df = pl.read_csv("MoviesTopRated.csv")
+    polars_vote_df = polars_movies_df.query("vote_average == vote_average.max()")
     print("Summary Statistics of the Top Rated Movies Dataset:\n")
-    print(movies_df.describe())
-    movies_df.plot.scatter(
+    print(polars_movies_df.describe())
+    polars_movies_df.plot.scatter(
         x="popularity",
         y="vote_average",
         title="popularity vs vote_average graph",
@@ -21,11 +20,11 @@ def display_highest_votes():
     )
     plt.show()
     print("\nDetails of the movies that were given the highest votes are: \n")
-    print(vote_df)
-    pdf_report_generator(movies_df)
-    return vote_df
+    print(polars_vote_df)
+    polars_report_generator(polars_movies_df)
+    return polars_vote_df
 
 
-def pdf_report_generator(df):
+def polars_report_generator(df):
     profile = ProfileReport(df, title="Summary Report")
-    profile.to_file("Summary_Report.html")
+    profile.to_file("Polars_Summary_Report.html")
