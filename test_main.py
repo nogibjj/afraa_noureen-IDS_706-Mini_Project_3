d88269cb7350458f5b3646de92c753661df886f0
afraa-n <143756865+afraa-n@users.noreply.github.com>
2023-09-17 21:31:38 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,12 +1,13 @@
 """test_main module"""
 import main
+movies_csv = "MoviesTopRated.csv"
 
 
 def test_main():
     """
     testing function for main
     """
-    polars_highest_votes = main.movies_statistics_polars()
+    polars_highest_votes = main.movies_statistics_polars(movies_csv)
     # print(highest_votes.loc[0,"vote_count"])
     assert polars_highest_votes["vote_count"][0] == 18448
     # print(highest_votes.loc[1,"vote_count"])
