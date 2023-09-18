2b5244b958b3a44b396103b88476d3bbe224b662
afraa-n <143756865+afraa-n@users.noreply.github.com>
2023-09-17 20:35:49 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -8,9 +8,9 @@ def test_main():
     """
     polars_highest_votes = main.movies_statistics_polars()
     # print(highest_votes.loc[0,"vote_count"])
-    assert polars_highest_votes.loc[0, "vote_count"] == 18448
+    assert polars_highest_votes.loc["vote_count"][0] == 18448
     # print(highest_votes.loc[1,"vote_count"])
-    assert polars_highest_votes.loc[1, "vote_count"] == 24376
+    assert polars_highest_votes.loc["vote_count"][1] == 24376
 
 
 if __name__ == "__main__":
